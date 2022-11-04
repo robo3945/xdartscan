5d527026317bd6ac6fb269e9209274d694420438
roberto <r.battistoni@gmail.com>
2022-11-04 08:08:54 +0100

    test

--- headers/config_manager.h
+++ headers/config_manager.h
@@ -2,6 +2,7 @@
 #define XDARTSCAN_CONFIG_MANAGER_H
 
 #include "file_signatures.h"
+#include <stdbool.h>
 
 int read_config_file(char* filename, bool verbose);
 void sort_signatures(MagicNumber* mn_array);
---
+++ testing/test_signatures.c
@@ -0,0 +1,12 @@
+#include "../headers/config_manager.h"
+
+void sort_signs();
+
+int main(int argc, char *argv[])
+{
+    sort_signs();
+}
+
+void sort_signs() {
+    sort_signatures(g_well_known_mn);
+}

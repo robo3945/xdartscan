e037a2975dfe8c10dc303ad4af7d6c072e037bf3
roberto <r.battistoni@gmail.com>
2025-01-05 19:48:59 +0100

    new version

--- logic/random_test.c
+++ logic/random_test.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
+
 #include "../headers/config.h"
 
 /**
@@ -11,7 +13,7 @@
  * @return
  *
  */
-double calc_rand_idx(const unsigned char *content, const long content_length) {
+double old_calc_rand_idx(const unsigned char *content, const long content_length) {
 
     // creates a bucket array to count the frequency of every char read from the file
     int* bucket  = calloc(MAX_SET_SIZE, sizeof(int));
@@ -53,6 +55,59 @@ double calc_rand_idx(const unsigned char *content, const long content_length) {
     }
 
     free(bucket);
+
+    return H;
+}
+
+/**
+ * Computes the Entropy factor
+ *
+ * @param content
+ * @param content_length
+ * @return
+ *
+ */
+double calc_rand_idx(const unsigned char *content, const long content_length) {
+    if (!content || content_length <= 0) {
+        return 0.0;
+    }
+
+    int bucket[MAX_SET_SIZE] = {0};
+
+    const unsigned char *end = content + content_length;
+    for (const unsigned char *p = content; p < end; p++) {
+        bucket[*p]++;
+    }
+
+    const double inv_length = 1.0 / content_length;
+
+    double H = 0.0;
+    for (int i = 0; i < MAX_SET_SIZE; i++) {
+        if (bucket[i]) {
+            const double fract = bucket[i] * inv_length;
+            H -= fract * log2(fract);
+        }
+    }
+
+    if (DEBUG_PRINT) {
+        printf("\n************************************************");
+        printf("\nSet: ");
+        for (int i = 0, j = 0; i < MAX_SET_SIZE; i++) {
+            if (bucket[i] > 0) {
+                printf("%d: ('%c', %d) - ", j++, i, bucket[i]);
+            }
+        }
+        printf("\nSet_length: %lu", content_length);
+        printf("\nSet with fract: ");
+        for (int i = 0, j = 0; i < MAX_SET_SIZE; i++) {
+            if (bucket[i] > 0) {
+                const double fract = bucket[i] * inv_length;
+                printf("%d: ('%c': %d, fract: %f) - ", j++, i, bucket[i], fract);
+            }
+        }
+        printf("\nCrypto values: H: %f", H);
+        printf("\n************************************************\n");
+    }
 
     return H;
 }
